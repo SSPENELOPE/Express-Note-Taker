08848b094bdd8a394bbcc06e460da63d7210bdd5
SSPENELOPE <typoepping@gmail.com>
2022-09-18 14:27:52 -0400

    added and modified server.js file

---
+++ server.js
@@ -0,0 +1,22 @@
+const { application } = require('express');
+const notes = require('./Develop/public/assets/js/index');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static('public'));
+
+
+app.get('/*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+)
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) =>
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+app.listen(PORT, () => console.log(`App Listening on http://localhost:${PORT} `))
