000f0dcb1d9430ecc7b4c9daac4bfcbceaae01fe
SSPENELOPE <typoepping@gmail.com>
2022-09-19 18:52:06 -0400

    heroku update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { application } = require('express');
 const notes = require('./Develop/public/assets/js/index');
+const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -18,5 +19,9 @@ app.get('/notes', (req, res) =>
 app.get('/api/notes', (req, res) =>
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
+
+/* app.post('/api/notes', (req, res) => 
+
+) */
 
 app.listen(PORT, () => console.log(`App Listening on http://localhost:${PORT} `))
