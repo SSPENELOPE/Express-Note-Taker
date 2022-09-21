9179d9986f141c0dda0bb9491ab50eb025c398b5
SSPENELOPE <typoepping@gmail.com>
2022-09-20 20:44:36 -0400

    added post route methods

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const { application } = require('express');
-const notes = require('./Develop/public/assets/js/index');
+const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -8,20 +8,23 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.get('/*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-)
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
+);
 
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) =>
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+  /*   req('./db/db.json').then((data) => res.json(JSON.parse(data))) */
+  console.log(res)
+    // req.somestuff.readfromfile
 );
 
-/* app.post('/api/notes', (req, res) => 
+/* app.post('/api/notes', (req, res) => {
+    // readAndAppend(notes,)   .('./db/db.json')
+}) */
 
-) */
 
 app.listen(PORT, () => console.log(`App Listening on http://localhost:${PORT} `))
