99c78222b7664f98ff2d342dd70c924d1400333f
SSPENELOPE <typoepping@gmail.com>
2022-09-20 22:33:43 -0400

    removed uncessary variables

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
-/* const { fstat } = require('fs'); */
 const fs = require('fs');
 const path = require('path');
-const db = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
