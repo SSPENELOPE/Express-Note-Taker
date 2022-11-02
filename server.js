ae91f55a87e6515f3a0f7d0b892ba70cc1bc3b33
SSPENELOPE <typoepping@gmail.com>
2022-11-01 20:25:40 -0400

    changed get route

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
+const notes = require('./db/db.json');
 
 const app = express();
 
 app.use(express.static('public'));
-
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
@@ -16,14 +17,20 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('/api/notes', (req, res) => 
+ app.get('/api/notes', (req, res) => 
     fs.readFile('/db/db.json', 'utf8', function(err, data){
-        res.json(JSON.parse(data));
+        if(err) {
+            console.log(err);
+            return;
+        } else {
+            res.json(notes);
+        }
    })
 );
 
 app.post('/api/notes', (req, res) => 
-    fs.writeFile('/db/db.json', 'utf8', function(err, data){
+    fs.appendFile('/db/db.json', 'utf8', function(err, data){
+        console.log(data)
         res.json(JSON.stringify(data));
 })
 )
